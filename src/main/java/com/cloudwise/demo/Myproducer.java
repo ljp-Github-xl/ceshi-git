3aaff127e10fc7e0968c52ea5e4ce47f54d42114
lijiangpeng <1597137969@qq.com>
2020-11-12 21:53:06 +0800

    测试git

--- src/main/java/com/cloudwise/demo/Myproducer.java
+++ src/main/java/com/cloudwise/demo/Myproducer.java
@@ -3,5 +3,6 @@ package com.cloudwise.demo;
 public class Myproducer {
     public static void main(String[] args) {
         System.out.println("测试git");
+        System.out.println("后台");
     }
 }
